c5f38c1af12971f9ba4da9a44028963348d20cd8
Jeremy Kendall <jkendall@opensky.com>
2013-06-07 22:10:47 -0500

    Completes adding composer
    * Removes __autoload function
    * Adds Inflection to composer file for autoloading
    * Adds composer autoloader to public/index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 define('ROOT_PATH', ROOT . DS . 'library' . DS . 'DEMO');
+
+require dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
 
 $url = (array_key_exists('url', $_GET) ? $_GET['url'] : null);
 require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
