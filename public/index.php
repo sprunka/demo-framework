4b5dbf5d0342e3e907d616fbc83128bca16ba46c
Jeremy Kendall <jkendall@opensky.com>
2013-06-09 11:47:36 -0500

    Removes url, moved closer to function in another commit

--- public/index.php
+++ public/index.php
@@ -6,5 +6,4 @@ define('ROOT_PATH', ROOT . DS . 'library' . DS . 'DEMO');
 
 require dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
 
-$url = (array_key_exists('url', $_GET) ? $_GET['url'] : null);
 require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
